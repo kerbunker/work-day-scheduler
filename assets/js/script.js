9db46e318f43bd7cec9af0ea533032c1cd98c9c3
Katelyn Bunker <kerbunker@gmail.com>
2021-11-06 21:29:19 -0400

    Past present future functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
 var currentDayEl = $("#currentDay");
-currentDayEl.text(moment().format('MMMM Do YYYY'));
-console.log(currentDayEl);
+var currentMoment = moment();
+currentDayEl.text(currentMoment.format('MMMM Do YYYY'));
+//console.log(currentDayEl);
+
+console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+var sevenOClock = moment().hour(10);
+console.log(sevenOClock);
+
+for (var i = 0; i < 9; i++) {
+    var setHour = moment().hour(18 + i);
+    var timeDifference = setHour.diff(currentMoment, 'hours', true);
+    if (timeDifference < 0) {
+        $("#hour-" + i).addClass('past');
+    } else if (timeDifference > 0.1) {
+        $("#hour-" + i).addClass('future');
+    } else {
+        $("#hour-" + i).addClass('present');
+    }
+}
