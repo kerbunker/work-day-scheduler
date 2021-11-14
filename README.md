005e21db1b8bbda3d4c7d15e9e7dab09af706695
Katelyn Bunker <kerbunker@gmail.com>
2021-11-14 07:10:04 -0500

    Add comments and README

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# work-day-scheduler
+# Work Day Scheduler
+
+## About this Planner
+
+ * Planner shows the current day and a time block for each hour in the normal 9-5 work day
+
+ * The time block for each hour shows a different color based on whether it is past present or future
+
+ * When a time block is clicked, the text can be edited
+
+ * When the save button for the time block is clicked the text in the time block is saved
+
+ * The added text is stored in local storage and persists between page reloads
+
+ * The text block background colors and the current day given are set to readjust periodically to change even if the page is not reloaded each hour or each day
+
+## Resources Used
+
+ * moment.js to get the current day and current moment to set the day and background colors
+
+ * bootstrap to set up the rows for the timeblocks
+
+ * jquery to simplify some of the functions to change the text on the page
+
+ * JSON to help store the data in local storage and load it back from local storage
+
+ * fontawesome for the save button icons
+
+ * open sans for the font used on the page
+
+## Links
+
+ * Github repository: [https://github.com/kerbunker/work-day-scheduler](https://github.com/kerbunker/work-day-scheduler)
+
+ * Deployed Site: [https://kerbunker.github.io/work-day-scheduler/](https://kerbunker.github.io/work-day-scheduler/)
+
+![webpage of the daily planner with added text and different colors per hour](/assets/images/page-screenshot.PNG)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,23 @@
-var currentDayEl = $("#currentDay");
-var currentMoment = moment();
-currentDayEl.text(currentMoment.format('MMMM Do YYYY'));
-//console.log(currentDayEl);
 
-console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
-var sevenOClock = moment().hour(10);
-console.log(sevenOClock);
+
+// stores the save data when hour event text has been added
 var eventData = [];
 
+// goes through each hour block and sets the background based on when it is in the day
 var setBackground = function() {
+    // variable to store the current day element
+    var currentDayEl = $("#currentDay");
+    // variable to store the current moment
+    var currentMoment = moment();
+    //sets the current day element to the current day in day month year format
+    currentDayEl.text(currentMoment.format('MMMM Do YYYY'));
+    
     for (var i = 0; i < 9; i++) {
+        // sets the hour to 9 - 5 depending on what i is
         var setHour = moment().hour(9 + i);
+        // gets the time difference between the hour in setHour and the current time
         var timeDifference = setHour.diff(currentMoment, 'hours', true);
+        // checks the time difference and sets the background color based on the time difference given
         if (timeDifference < 0) {
             $("#hour-" + i).addClass('past');
         } else if (timeDifference > 0.1) {
@@ -22,14 +28,16 @@ var setBackground = function() {
     }
 };
 
-
+// when a time block is clicked the div turns into a text area
 $(".time-block").on("click", ".text-block", function() {
-    console.log("clicked");
+    // gets the current text
     var text = $(this).text().trim();
+    // saves the ID for the div
     var divID = $(this).attr("id");
-    console.log(divID);
 
+    // creates the text area adding the classes and the ID from the div
     var textInput = $("<textarea>").addClass("col-10 text-form").attr("id", divID).val(text);
+    // adds the class for what background color it should have
     if ($(this).hasClass("past")) {
         textInput.addClass("past");
     } else if($(this).hasClass("present")) {
@@ -37,21 +45,24 @@ $(".time-block").on("click", ".text-block", function() {
     } else {
         textInput.addClass("future");
     }
+    //replaces the div with the input form
     $(this).replaceWith(textInput);
     setBackground();
 
     textInput.trigger("focus");
 });
 
+// Reverts the input form to a div when the save button is clicked
 $(".time-block").on("click", ".saveBtn", function() {
     // checks if the div to save was a text form to change text, otherwise, nothing needs to be changed
     if (!$(this).prev().hasClass("text-form")) {
         return;
     }
+    // gets the text and ID from the input form
     var text = $(this).prev().val();
     var textID = $(this).prev().attr("id");
-    //console.log(text);
 
+    // creates the div with the classes ID and text from the input form
     var descP = $("<div>").addClass("col-10 text-block").attr("id", textID).text(text);
     if ($(this).prev().hasClass("past")) {
         descP.addClass("past");
@@ -60,8 +71,11 @@ $(".time-block").on("click", ".saveBtn", function() {
     } else {
         descP.addClass("future");
     }
+    // replaces the input form with the created div
     $(this).prev().replaceWith(descP);
     setBackground();
+
+    // saves the data in the eventData array
     var index = $(this).prev().attr("id");
     index = index.substr(index.length-1, 1);
     index = parseInt(index);
@@ -76,10 +90,12 @@ $(".time-block").on("click", ".saveBtn", function() {
     saveData();
 });
 
+// saves the Data in local storage as a string
 var saveData = function() {
     localStorage.setItem("data", JSON.stringify(eventData))
 };
 
+// loads the data from local storage and creates an array of objects and loads them onto the page
 var loadData = function() {
     eventData = JSON.parse(localStorage.getItem("data"));
     if (!eventData) {
@@ -92,5 +108,9 @@ var loadData = function() {
     }
 }
 
+// set interval to refresh hour background periodically throughout the day even if the page is not reloaded
+setInterval(setBackground, 600000);
+// sets the day and background colors on page load
 setBackground();
+// loads the data from local storage on page load
 loadData();
